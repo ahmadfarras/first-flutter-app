8a6afd9f77e4352b019465b32cb84cd8bff4974f
Farras <ahmadfarrassyafrin@gmail.com>
2024-02-11 07:57:18 +0700

    Add UI to center

--- lib/main.dart
+++ lib/main.dart
@@ -41,15 +41,17 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random awesome idea:'),
-          BigCard(pair: pair),
-
-          ElevatedButton(onPressed: (){
-            appState.getNext();
-          }, child: Text('Next'))
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: (){
+              appState.getNext();
+            }, child: Text('Next'))
+          ],
+        ),
       ),
     );
   }
